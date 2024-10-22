90865ce2766c34df2223691722400f9d1335ec69
cyprien <cypriengehu@gmail.com>
2024-10-22 12:41:22 +0000

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabel - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	_putchar('\n');
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -1,1 +1,2 @@
-0-putchar.c: The program prints "_putchar" 
+0-putchar.c: The program prints "_putchar"_
+1-alphabet.c: The function print the alphabet, in lowercase
