b482c89e0f38bd9feba6f844a2c7332824716c33
cyprien <cypriengehu@gmail.com>
2024-10-22 13:49:47 +0000

    task3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Return 0 and Return 1
+ * @c : 'c' is a ASCII decimal
+ *
+ * Return: 1 if is a lowercase, _islower return 0 if is a uppercase
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 65 && c <= 95)
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
