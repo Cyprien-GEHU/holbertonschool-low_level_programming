dd42c864399f12edba3c4ff125c73f49ab907cd6
cyprien <cypriengehu@gmail.com>
2024-10-29 16:21:39 +0000

    task10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,19 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int valeur;
+	int signe = 1;
 
 	while(*s)
 	{
+		if (s[i] == '-')
+			signe *= -1;
+
 		if (s[i] >= '0' && s[i] <= '9')
 			valeur = s[i];	
 		s++;
 		i++;
 	}
+	valeur = valeur * signe;
 
 	return (valeur);
 }
