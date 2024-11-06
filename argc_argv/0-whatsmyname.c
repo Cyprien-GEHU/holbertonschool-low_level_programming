124a9e532441c218f93b0ba8222bf059c1a7f3ea
cyprien <cypriengehu@gmail.com>
2024-11-06 09:35:30 +0000

    task0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * main - print the name program
+ * @argc : the size of argv
+ * @argv : the argument with program
  *
+ * Return: 0;
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	argc = 0;
 	printf("%s\n", argv[argc]);
