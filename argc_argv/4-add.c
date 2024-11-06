63d69f8848a6f18321497f42db695b220a3920f1
cyprien <cypriengehu@gmail.com>
2024-11-06 10:22:40 +0000

    task4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - add all postif number
+ * @argc : the size of argv
+ * @argv : the argument with program
+ *
+ * Return: 0;
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int result = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!(atoi(argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (atoi(argv[i]) < 0)
+			result += 0;
+		else
+			result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
