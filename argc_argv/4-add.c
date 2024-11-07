5d8de568606db771e9ef1ad811bcfd3f84d0161f
cyprien <cypriengehu@gmail.com>
2024-11-07 14:05:19 +0000

    task4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,7 +13,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
+	int i;
+	int n;
+	int add = 0;
 	int result = 0;
 
 	if (argc <= 1)
@@ -29,12 +31,17 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		if (atoi(argv[i]) < 0)
-			result += 0;
 		else
-			result += atoi(argv[i]);
+		{
+			for (n = 0; argv[i][n] != '\0'; n++)
+			{
+				if (argv[i][n] >= '0' && argv[i][n] <= '9')
+					add = add * 10 + argv[i][n] - '0';
+			}
+			result += add;
+			add = 0;
+		}
 	}
-
 	printf("%d\n", result);
 	return (0);
 }
