745002a129d3b91096b5889752d54088215c0eab
cyprien <cypriengehu@gmail.com>
2024-11-12 10:04:10 +0000

    task2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - create an arrayof chars
+ * @s1 : first string
+ * @s2 : second string
+ *
+ * Return: char or "NULL";
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int x;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	arr = malloc((len1 + len2) * sizeof(char) + 1);
+	for (x = 0; x < len1; x++)
+		arr[x] = s1[x];
+	for (x = 0; x < len2; x++)
+		arr[len1 + x] = s2[x];
+
+	arr[len1 + len2 + 1] = '\0';
+	return (arr);
+}
