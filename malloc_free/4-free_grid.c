3e4e062dd134328a456883c41ff1a104c223015d
cyprien <cypriengehu@gmail.com>
2024-11-12 13:53:02 +0000

    task4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a dimensional
+ * @grid : first string
+ * @height : size of grid
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int n;
+
+	for (n = 0; n < height; n++)
+		free(grid[n]);
+}
