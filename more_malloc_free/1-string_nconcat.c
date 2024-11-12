fd1a75892f31c392a62973aba088466c9c5efcf1
cyprien <cypriengehu@gmail.com>
2024-11-12 14:47:15 +0000

    task1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two string
+ * @s1 : first string
+ * @s2 : second string
+ * @n : byte
+ *
+ * Return : new string or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+}
