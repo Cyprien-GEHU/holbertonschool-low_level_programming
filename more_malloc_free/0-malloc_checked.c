82af6d8d31f8bdd8af7e033d7569842d38057493
cyprien <cypriengehu@gmail.com>
2024-11-12 15:12:30 +0000

    task0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,8 +5,19 @@
  * malloc_checked - allocates memory
  * @b : length of the srting
  *
+ * Return: loc or exit(98) 
  */
 
 void *malloc_checked(unsigned int b)
 {
+
+	void *loc = malloc(b);
+
+	if (b > 0)
+	{
+		if (loc == 0)
+			exit(98);
+		return (loc);
+	}
+	exit(98);
 }
