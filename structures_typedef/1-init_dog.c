2d5a155da9327c0f803367e26165d0f67000c665
cyprien <cypriengehu@gmail.com>
2024-11-13 10:55:00 +0000

    task1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * init_dog - initialize
+ * @d : the struct
+ * @name : the name
+ * @age : the age
+ * @owner : the owner
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if(d)
+	{
+		(*d).name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
