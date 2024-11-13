fe05a98c82c0b7a1dbf1b3495615bbb8986f6661
cyprien <cypriengehu@gmail.com>
2024-11-13 16:34:35 +0000

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,13 +27,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	if (n <= 0)
-		return (NULL);
-
 	if (n < len2)
 		len2 = n;
 
-	arr = malloc(len1 + len2 + 1);
+	arr = malloc(sizeof(char) * len1 + len2 + 1);
 	if (arr == NULL)
 	{
 		free(arr);
