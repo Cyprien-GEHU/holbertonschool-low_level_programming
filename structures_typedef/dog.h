9be39f8bb8fd4f495fd920c830711297bc8fe1ea
cyprien <cypriengehu@gmail.com>
2024-11-13 10:28:56 +0000

    task0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_FILE
 #define DOG_FILE
 /**
- * struct dog - the Struct type for scooby 
+ * struct dog - the Struct type for scooby
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the name of dog's owner
