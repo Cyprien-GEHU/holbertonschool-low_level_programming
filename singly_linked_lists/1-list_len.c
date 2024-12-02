9b7dcac656e9c0c5f846ef981385eb90e28ae18a
cyprien <cypriengehu@gmail.com>
2024-12-02 10:20:13 +0000

    task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- * print_list - print all element of a list
+ * list_len - return number of element
  * @h : the content of element
  *
- * Return: node printed
+ * Return: element
  */
 
 size_t list_len(const list_t *h)
 {
 	int x = 0;
+
 	while (h)
 	{
 		h = h->next;
