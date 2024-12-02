cb71c6de3f649d32bda4d77f34918008d4890d2d
cyprien <cypriengehu@gmail.com>
2024-12-02 10:20:25 +0000

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,12 +10,13 @@
 size_t print_list(const list_t *h)
 {
 	int x = 0;
+
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n",h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		x++;
 	}
