d0aeb5dcac498e494ec7196cb306a039435b4469
cyprien <cypriengehu@gmail.com>
2024-12-02 10:12:37 +0000

    task1

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print all element of a list
+ * @h : the content of element
+ *
+ * Return: node printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	int x = 0;
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n",h->len, h->str);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
