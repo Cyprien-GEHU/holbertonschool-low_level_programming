72da6301011e801aa56a9e2ebcfed86f10aae5c4
cyprien <cypriengehu@gmail.com>
2024-12-05 10:30:02 +0000

    task0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,8 +10,9 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, n;
-	char *buf[1024];
+	int file; 
+	size_t n;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
@@ -20,11 +21,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (file == -1)
 		return (0);
+	buf = malloc(letters + 1);
+
+	if (buf == NULL)
+		return (0);
 
 	n = read(file, buf, letters);
-
-	if (n == -1)
-		return (0);
 
 	write(STDOUT_FILENO, buf, n);
 	close(file);
