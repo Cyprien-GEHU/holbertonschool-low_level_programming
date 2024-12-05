9fc0081fe409b1ae3018a2a86277a720a7cc42d0
cyprien <cypriengehu@gmail.com>
2024-12-05 16:09:35 +0000

    task3

---
+++ file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+
+/**
+ * copy_function - function copy
+ * @fm : file source
+ * @ft : file destination
+ */
+
+void copy_function(char *fm, char *ft)
+{
+	int f_from = open(fm, O_RDONLY);
+	int f_to = open(ft, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	ssize_t copy, wrote;
+	char b[1024];
+
+	if (f_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fm);
+		exit(98);
+	}
+	if (f_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", ft);
+		exit(99);
+	}
+	while ((copy = read(f_from, b, 1024)) > 0)
+	{
+		wrote = write(f_to, b, copy);
+		if (copy != wrote)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read to file %s\n", ft);
+			exit(98);
+		}
+	}
+	if (copy == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fm);
+		exit(98);
+	}
+	if (close(f_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f_from);
+		exit(100);
+	}
+	if (close(f_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", f_to);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac : number of argument
+ * @av : argument
+ *
+ * Return: 0
+ */
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	else
+		copy_function(av[1], av[2]);
+
+	return (0);
+}
