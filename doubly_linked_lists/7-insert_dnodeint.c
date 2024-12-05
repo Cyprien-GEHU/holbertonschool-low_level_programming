31f77c5ab020dfeb2b864c25acd9d247b6d7eca9
cyprien <cypriengehu@gmail.com>
2024-12-05 16:33:46 +0000

    task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,12 +11,11 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int index = 0;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *tempo = *h;
 
 	if (idx == 0)
-		return (add_dnodeint(h,n));
+		return (add_dnodeint(h, n));
 
 	for (; idx != 1; idx--)
 	{
@@ -26,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (tempo->next == NULL)
-		return(add_dnodeint_end(h, n));
+		return (add_dnodeint_end(h, n));
 
 	if (new_node == NULL)
 		return (NULL);
@@ -34,8 +33,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = tempo->next;
 	new_node->prev = tempo;
-	temp->next->prev = new_node;
-	tempo->next = new_node
+	tempo->next->prev = new_node;
+	tempo->next = new_node;
 
 	return (new_node);
 }
