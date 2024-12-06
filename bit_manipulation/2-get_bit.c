ee8d9dfe03c59c6a4fd1bf3f241c5f731c5d06cc
cyprien <cypriengehu@gmail.com>
2024-12-06 12:01:55 +0000

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert bit to decimal
- * @b : chain of character
+ * get_bit - return the value of a bit at given index
+ * @n : value
+ * @index :the index
  *
  * Return: 0 or 1 or -1 if we have an error
  */
@@ -14,5 +15,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	t = 1 << index;
-	return((n & t) != 0);
+	return ((n & t) != 0);
 }
